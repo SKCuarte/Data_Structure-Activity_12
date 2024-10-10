9865a57ae3e1bbb9de23e252ec67c93dbd2b968a
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 00:50:12 +0800

    game_list

---
+++ game_list.py
@@ -0,0 +1,37 @@
+#1. Create a list of 15 video game titles.
+game_list = ["The Legend of Zelda: Ocarina of Time", "SoulCalibur", "Grand Theft Auto IV", "Super Mario Galaxy", "Super Mario Galaxy 2", "The Legend of Zelda: Breath of the Wind", "Perfect Dark", "Tony Hawk's Pro Skater 3", "Red Dead Redemption 2", "Grand Theft Auto V", "Metroid Prime", "Grand Theft Auto III", "Super Mario Odyssey", "Halo: Combat Evolved", "NFL 2K1"]
+
+#2. Print the entire list.
+print(game_list)
+
+#3. Access and print the 7th index.
+print(game_list[6])
+
+#4. Update the 4th index to "Minecraft".
+game_list[3] = "Minecraft"
+print(game_list)
+
+#5. Delete the 9th index.
+del game_list[8]
+print(game_list)
+
+#6. Slice the list from index 5 to 10.
+print(game_list[4:9])
+#OR
+print(game_list[4:10])
+
+#7. Print the last index.
+print(game_list[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
