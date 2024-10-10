40d0abccadb9cec1b764de3a554156a2e789beac
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 01:38:54 +0800

    flower_list

---
+++ flower_list.py
@@ -0,0 +1,21 @@
+#1. Create a list of 10 flower names.
+flower_list = ["Amaranth", "Tulips", "Daisy", "Sunflower", "Lily", "Lotus", "Orchid", "Violet", "Daffodil", "Marigold"] 
+
+#2. Print the entire list.
+print(flower_list)
+
+#3. Access and print the 5th index.
+print(flower_list[4])
+
+#4. Update the 8th index to "Rose".
+flower_list[7] = "Rose"
+print(flower_list)
+
+#5. Delete the 2nd index.
+del flower_list[1]
+print(flower_list)
+
+#6. Print the last index.
+print("The last index is: ", flower_list[-1])
+
+
