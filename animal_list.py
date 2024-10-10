c73cf55ba7af864aa5c6ade11e9b19388782f95f
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-10 23:07:14 +0800

    animal_list.py

---
+++ animal_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 animal names.
+animal_names = ["Ant", "Cow", "Chicken", "Dog", "Cat", "Mouse", "Frog", "Rabbit", "Lizard", "Butterfly"]
+
+#2. Print the entire list.
+print(animal_names)
+
+#3. Access and print the 3rd index.
+print(animal_names[2])
+
+#4. Update the 6th index to "Elephant".
+animal_names[5] = "Elephant"
+print(animal_names)
+
+#5. Delete the 8th index.
+del animal_names[7]
+print(animal_names)
+
+#6. Print the last index.
+print(animal_names[-1])
