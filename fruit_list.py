9c124e380fdf28e65db208a5f09903ff46d1e1a3
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-10 23:52:18 +0800

    fruit_list

---
+++ fruit_list.py
@@ -0,0 +1,26 @@
+
+#1. Create a list of 12 fruits.
+fruit_list = ["Avocado", "Coconut", "Guava", "Limoncillo", "Apple", "Kiwi", "Papaya", "Pineapple", "Pitaya", "Plantain", "Soursop", "Starfruit"]
+
+#2. Print the entire list.
+print(fruit_list)
+
+#3. Access and print the 9th index.
+print(fruit_list[8])
+
+#4. Update the 2nd index to "Mango".
+fruit_list[1] = "Mango"
+print(fruit_list)
+
+#5. Delete the 10th index.
+del fruit_list[9]
+print(fruit_list)
+
+#6. Slice the list from index 4 to 7.
+print(fruit_list[3:7])
+#OR
+print(fruit_list[3:8])
+
+#7. Print the last index.
+print(fruit_list[-1])
+
