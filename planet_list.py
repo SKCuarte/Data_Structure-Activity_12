1adf60700f0f980dcc13122fd9daa66125997b7b
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 01:19:31 +0800

    planet_list

---
+++ planet_list.py
@@ -0,0 +1,22 @@
+#1. Create a list of 8 planet names.
+planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+#2. Print the entire list.
+print(planet_list)
+
+#3. Access and print the 3rd index.
+print(planet_list[2])
+
+#4. Update the 7th index to "Pluto".
+planet_list[6] = "Pluto"
+print(planet_list)
+
+#5. Delete the 4th index.
+del planet_list[3]
+print(planet_list)
+
+#6. Print the last index.
+print("The last index is: ", planet_list[-1])
+
+
+ 
