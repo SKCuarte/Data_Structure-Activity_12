dcc2bd770984a6db611f80e260fc0a5b085ce704
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 01:30:53 +0800

    language_list

---
+++ language_list.py
@@ -0,0 +1,38 @@
+#1. Create a list of 14 programming languages.
+language_list = ["C", "C++", "Java", "C#", "Visual Basic .NET", "JavaScript", "SQL", "Assembly language", "PHP", "R", "Go", "Classic Visual Basic", "MATLAB", "Swift"]
+
+#2. Print the entire list.
+print(language_list)
+
+#3. Access and print the 11th index.
+print(language_list[10])
+
+#4. Update the 9th index to "Python".
+language_list[8] = "Python"
+print(language_list)
+
+#5. Delete the 13th index.
+del language_list[12]
+print(language_list)
+
+#6. Slice the list from index 5 to 12.
+print(language_list[4:11])
+#OR
+print(language_list[4:12])
+
+#7. Print the last index.
+print("The last index is: ", language_list[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
