8c3e244ed2ca858e7c0614ecfcf063f97d71d4a8
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 00:03:46 +0800

    vegetable_list

---
+++ vegetable_list.py
@@ -0,0 +1,20 @@
+#1. Create a list of 7 vegetables.
+vegetable_list = ["Carrot", "Broccoli", "Garlic", "Brussel Sprouts", "Kale", "Potato", "Asparagus"]
+
+#2. Print the entire list.
+print(vegetable_list)
+
+#3. Access and print the 5th index.
+print(vegetable_list[4])
+
+#4. Update the 3rd index to "Spinach".
+vegetable_list[2] = "Spinach"
+print(vegetable_list)
+
+#5. Delete the 6th index.
+del vegetable_list[5]
+print(vegetable_list)
+
+#6. Print the last index.
+print(vegetable_list[-1])
+
