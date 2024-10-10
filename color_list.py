0cd1dae52a99d24f40ca687dfd27f64d135bcf88
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-10 23:39:54 +0800

    color_list

---
+++ color_list.py
@@ -0,0 +1,24 @@
+#1. Create a list of 15 colors.
+color_list = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Aqua", "Black", "Grey", "White","Maroon", "Brown", "Cyan", "Teal", "Gold"]
+
+#2. Print the entire list.
+print(color_list)
+
+#3. Access and print the 10th index.
+print(color_list[9])
+
+#4. Update the 4th index to "Purple".
+color_list[3] = "Violet"
+print(color_list)
+
+#5. Delete the 5th index.
+del color_list[4]
+print(color_list)
+
+#6. Slice the list from index 2 to 8.
+print(color_list[2:8])
+#OR
+print(color_list[2:9])
+
+#7. Print the last index.
+print(color_list[-1])
