0a20c52ba0288b1ad050283a82e67469ba0ac032
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 17:22:57 +0800

    actor_list

---
+++ actor_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 famous actors.
+actor_list = ["Denzel Washington", "Robert De Niro", "Al Pacino", "Morgan Freeman", "Jack Nicholson", "Robert Downey Jr.", "Johnny Depp", "Tom Cruise", "Brad Pitt", "Harrison Ford"]
+
+#2. Print the entire list.
+print(actor_list)
+
+#3. Access and print the 7th index.
+print(actor_list[6])
+
+#4. Update the 3rd index to "Leonardo DiCaprio".
+actor_list[2] = "Leonardo DiCarpio"
+print(actor_list)
+
+#5. Delete the 8th index.
+del actor_list[7]
+print(actor_list)
+
+#6. Print the last index.
+print("The last index is: ", actor_list[-1])
