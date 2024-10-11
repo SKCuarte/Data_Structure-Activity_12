76914cce56fefb56c4911532b867bf4ea96205c1
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 12:04:08 +0800

    animal_species_list

---
+++ animal_species_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 7 animal species.
+animal_species_list = ["Dog", "Cow", "Cat", "Horse", "Donkey", "Tiger", "Lion"]
+
+#2. Print the entire list. 
+print(animal_species_list)
+
+#3. Access and print the 2nd index.
+print(animal_species_list[1])
+
+#4. Update the 5th index to "Gorilla".
+animal_species_list[4] = "Gorilla"
+print(animal_species_list)
+
+#5. Delete the 6th index.
+del animal_species_list[5]
+print(animal_species_list)
+
+#6. Print the last index.
+print("The last index is: ", animal_species_list[-1])
