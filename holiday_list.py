0f2ff333939ccf814024b058c50f5b5363e01b48
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 12:51:30 +0800

    holiday_list

---
+++ holiday_list.py
@@ -0,0 +1,27 @@
+#1. Create a list of 12 holidays.
+holiday_list = ["Valentine's Day", "Easter", "Thanksgiving", "St. Patrick's Day", "Halloween", "New Year", "Chinese New Year", "Independence Day", "Oktoberfest", "Hanukkah", "Ramadan", "Diwali"]
+
+#2. Print the entire list.
+print(holiday_list)
+
+#3. Access and print the 9th index.
+print(holiday_list[8])
+
+#4. Update the 3rd index to "Christmas".
+holiday_list[2] = "Christmas"
+print(holiday_list)
+
+#5. Delete the 11th index.
+del holiday_list[10]
+print(holiday_list)
+
+#6. Slice the list from index 2 to 7.
+print(holiday_list[1:6])
+#OR
+print(holiday_list[1:7])
+
+#7. Print the last index
+print("The last index is: ", holiday_list[-1])
+
+
+
