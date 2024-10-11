45584d461b123465341640a5fdfdb0b45ec05966
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 11:23:16 +0800

    company_list

---
+++ company_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 company names.
+company_list = ["Alphabet", "Walmart", "Amazon", "Facebook", "Shell", "Tesla", "American Express", "Nvidia", "Meta", "Microsoft"]
+
+#2. Print the entire list.
+print(company_list)
+
+#3. Access and print the 7th index.
+print(company_list[6])
+
+#4. Update the 5th index to "Apple".
+company_list[4] = "Apple"
+print(company_list)
+
+#5. Delete the 8th index.
+del company_list[7]
+print(company_list)
+
+#6. Print the last index.
+print("The last index is: ", company_list[-1])
