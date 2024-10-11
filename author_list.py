c0108aea554446f06a04c9c331b8cee4c0223f40
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 10:04:03 +0800

    author_list

---
+++ author_list.py
@@ -0,0 +1,31 @@
+#1. Create a list of 12 famous authors.
+author_list = ["William Shakespeare", "Homer", "Virgil", "Leo Tolstoy", "Ernest Hemingway", "Jane Austen", "Charles Dickens", "Fyodor Dostoevsky", "Virginia Woolf", "Geoffrey Chaucer", "Francois Rabelais", "Sophocles"]
+
+#2. Print the entire list.
+
+print(author_list)
+
+#3. Access and print the 6th index.
+print(author_list[5])
+
+#4. Update the 4th index to "Mark Twain".
+author_list[3] = "Mark Twain"
+print(author_list)
+
+#5. Delete the 10th index.
+del author_list[9]
+print(author_list)
+
+#6. Slice the list from index 3 to 8.
+print(author_list[2:7])
+#OR
+print(author_list[2:8])
+
+#7. Print the last index.
+print(author_list[-1])
+
+
+
+
+
+
