18ff4d4111d250f69ebd828970541abe9887d22a
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 16:53:38 +0800

    sports_list

---
+++ sports_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 popular sports.
+sports_list = ["Aquatics", "Archery", "Badmintod", "Cycling", "Fencing", "Football", "Golf", "Hockey", "Lacrosse", "Rugby"]
+
+#2. Print the entire list.
+print(sports_list)
+
+#3. Access and print the 6th index.
+print(sports_list[5])
+
+#4. Update the 4th index to "Basketball".
+sports_list[3] = "Basketball"
+print(sports_list)
+
+#5. Delete the 9th index.
+del sports_list[8]
+print(sports_list)
+
+#6. Print the last index.
+print("The last index is: ", sports_list[-1])
