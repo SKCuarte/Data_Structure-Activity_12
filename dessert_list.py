f8f58d77d9e1b53efb92bc9c896b6959bfca0c9a
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 13:11:05 +0800

    dessert_list

---
+++ dessert_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 desserts.
+dessert_list = ["Puddings", "Pies", "Custards", "Creams", "Meringues", "Crepes", "Pancakes", "Waffles", "Cream puffs", "Marzipan"]
+
+#2. Print the entire list.
+print(dessert_list)
+
+#3. Access and print the 4th index.
+print(dessert_list[3])
+
+#4. Update the 6th index to "Chocolate Cake".
+dessert_list[5] = "Chocolate Cake"
+print(dessert_list)
+
+#5. Delete the 3rd index.
+del dessert_list[2] 
+print(dessert_list)
+
+#6. Print the last index.
+print("the last index is: ", dessert_list[-1])
