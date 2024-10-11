c7c8b1170b8eeeb23928bccc9fb60e3625fa19e4
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 16:46:38 +0800

    social_media_list

---
+++ social_media_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 social media platforms.
+social_media_list = ["Facebook", "Whatsapp", "Youtube", "QQ", "WeChat", "Tiktok", "Telegram", "Snapchat", "Kuaishou", "Sina Weibo"]
+
+#2. Print the entire list.
+print(social_media_list)
+
+#3. Access and print the 5th index.
+print(social_media_list[4])
+
+#4. Update the 3rd index to "Instagram".
+social_media_list[2] = "Instagram"
+print(social_media_list)
+
+#5. Delete the 8th index.
+del social_media_list[7]
+print(social_media_list)
+
+#6. Print the last index.
+print("The last index is: ", social_media_list[-1])
