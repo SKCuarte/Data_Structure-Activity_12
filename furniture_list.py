1f65e567857ec157efb6f1cb04995853b0f80cd9
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 16:40:03 +0800

    furniture_list

---
+++ furniture_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 12 furniture items.
+furniture_list = ["Benches", "Traffic barriers", "Bollards", "Post boxes", "Phone boxes", "Streetlamps", "Traffic lights", "Traffic signs", "Bus stops", "Tram stops", "Taxi stands", "Public lavatories"]
+
+#2. Print the entire list.
+print(furniture_list)
+
+#3. Access and print the 8th index.
+print(furniture_list[7])
+
+#4. Update the 5th index to "Sofa".
+furniture_list[4] = "Sofa"
+print(furniture_list)
+
+#5. Delete the 10th index.
+del furniture_list[9]
+print(furniture_list)
+
+#6. Print the last index.
+print("The last index is: ", furniture_list[-1])
