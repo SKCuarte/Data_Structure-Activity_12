057d444beb19ce09ea69b9cf84c5ed70d7bdb8e5
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 17:14:52 +0800

    building_list.py

---
+++ building_list.py
@@ -0,0 +1,23 @@
+#1. Create a list of 8 famous buildings.
+building_list = ["Taj Mahal", "Colosseum", "St. Basil's Cathedral", "Sydney Opera House", "Burj Khalifa", "Lotus Temple", "Pantheon", "Chrysler Building"]
+
+#2. Print the entire list.
+print(building_list)
+
+#3. Access and print the 5th index.
+print(building_list[4])
+
+#4. Update the 2nd index to "Eiffel Tower".
+building_list[1] = "Eiffel Tower"
+print(building_list)
+
+#5. Delete the 6th index.
+del building_list[5]
+print(building_list)
+
+#6. Print the last index.
+print("The last index is: ", building_list[-1])
+
+
+
+
