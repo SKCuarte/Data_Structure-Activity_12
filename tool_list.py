2ed04677abc7b995809bd0c19732c1a2aa5193e0
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 17:38:21 +0800

    tool_list

---
+++ tool_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 tools used in carpentry.
+tool_list = ["Saw", "Screwdriver", "Wrench", "Tape Measure", "Ladder", "Scissor", "Axe", "Level", "Mallet", "Handsaw"]
+
+#2. Print the entire list.
+print(tool_list)
+
+#3. Access and print the 4th index.
+print(tool_list[3])
+
+#4. Update the 7th index to "Hammer".
+tool_list[6] = "Hammer"
+print(tool_list)
+
+#5. Delete the 5th index.
+del tool_list[4]
+print(tool_list)
+
+#6. Print the last index.
+print("The last index is: ", tool_list[-1])
