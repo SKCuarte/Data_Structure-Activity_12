2bc30db80973631ab723677fd50021938acc16dd
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 11:04:08 +0800

    mountain_list

---
+++ mountain_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 8 mountain names.
+mountain_list = ["K2", "Kangchenjunga", "Lhotse", "Makalu", "Cho Oyu", "Dhaulagiri", "Manaslu", "Nanga Parbat"]
+
+#2. Print the entire list.
+print(mountain_list)
+
+#3. Access and print the 5th index.
+print(mountain_list[4])
+
+#4. Update the 3rd index to "Everest".
+mountain_list[2] = "Everest"
+print(mountain_list)
+
+#5. Delete the 6th index.
+del mountain_list[5]
+print(mountain_list)
+
+#6. Print the last index.
+print("the last index is: ", mountain_list[-1])
