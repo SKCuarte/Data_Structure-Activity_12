89d44e394c74bd412c2969be5201af33c33a2c60
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 16:32:49 +0800

    continent_list

---
+++ continent_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 7 continents.
+continent_list = ["Asia", "North America", "Africa", "South America", "Antarctica", "Europe", "Australia"]
+
+#2. Print the entire list.
+print(continent_list)
+
+#3. Access and print the 4th index.
+print(continent_list[3])
+
+#4. Update the 2nd index to "Africa".
+continent_list[1] = "Africa"
+print(continent_list)
+
+#5. Delete the 6th index.
+del continent_list[5]
+print(continent_list)
+
+#6. Print the last index.
+print("The last index is: ", continent_list[-1])
