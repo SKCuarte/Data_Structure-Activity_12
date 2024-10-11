03dc43d7bbc2d3654f7739eed16219556eb6c82c
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 12:19:17 +0800

    historical_figures_list

---
+++ historical_figures_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 historical figures.
+historical_figures_list = ["Jesus", "Napoleon", "Muhammad", "William Shakespeare", "Abraham Lincoln", "George Washington", "Adolf Hitler", "Aristotle", "Alexander the Great", "Thomas Jefferson"]
+
+#2. Print the entire list.
+print(historical_figures_list)
+
+#3. Access and print the 8th index.
+print(historical_figures_list[7])
+
+#4. Update the 4th index to "Nelson Mandela".
+historical_figures_list[3] = "Nelson Mandela"
+print(historical_figures_list)
+
+#5. Delete the 7th index.
+del historical_figures_list[6]
+print(historical_figures_list)
+
+#6. Print the last index.
+print("The last index is: ", historical_figures_list[-1])
