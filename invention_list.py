43bbded764f865e7132f6b0d02ab60d4563e125d
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-10-11 12:33:57 +0800

    invention_list

---
+++ invention_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 8 famous inventions.
+invention_list = ["Wheel", "Nail", "Penicillin", "Cement", "The Steam Engine", "Paper", "The Plow", "Vaccines"]
+
+#2. Print the entire list.
+print(invention_list)
+
+#3. Access and print the 6th index.
+print(invention_list[5])
+
+#4. Update the 2nd index to "Lightbulb".
+invention_list[1] = "Lightbulb"
+print(invention_list)
+
+#5. Delete the 5th index.
+del invention_list[4]
+print(invention_list)
+
+#6. Print the last index.
+print("the last index is: ", invention_list[-1])
