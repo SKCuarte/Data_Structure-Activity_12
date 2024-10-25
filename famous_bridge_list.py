2a69986333182e15363204bbf027982ce2a174e0
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:04:10 +0800

    famous_bridge_list

---
+++ famous_bridge_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 8 famous bridges.
+bridge_list = ["Sydney Harbour Bridge", "Tower Bridge", "Brooklyn Bridge", "Charles Bridge", "Si-o-Se Pol Bridge", "Ponte di Rialto", "Chapel Bridge", "Pont du Gard"]
+
+#2. Print the entire list.
+print(bridge_list)
+
+#3. Access and print the 3rd index.
+print(bridge_list[2])
+
+#4. Update the 6th index to "Golden Gate Bridge".
+bridge_list[5] = "Golden gate Bridge"
+print(bridge_list)
+
+#5. Delete the 7th index.
+del bridge_list[6]
+print(bridge_list)
+
+#6. Print the last index.
+print("The last Index is: ", bridge_list[-1])
