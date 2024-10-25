915970f3c8a79ab22a31d009e75ead9e7d2ba727
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:58:30 +0800

    currency_list

---
+++ currency_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 world currencies.
+currency_list = ["Kuwaiti Dinar", "Bahraini Dinar", " Jordanian Dinar", "Pound Streling", "Cayman Islands Dollar", "US Dollar", "Canadian Dollar", "Brunei Dollar", "Singapore Dollar", "Bulgarian Lev"]
+
+#2. Print the entire list.
+print(currency_list)
+
+#3. Access and print the 4th index.
+print(currency_list[3])
+
+#4. Update the 7th index to "Euro".
+currency_list[6] = "Euro"
+print(currency_list)
+
+#5. Delete the 9th index.
+del currency_list[8]
+print(currency_list)
+
+#6. Print the last index.
+print("The last index is: ", currency_list[-1])
