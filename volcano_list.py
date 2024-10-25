3fbf7982c6b5a715c9d1d9915a22f8d688af74c6
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:03:21 +0800

    volcano_list

---
+++ volcano_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 7 famous volcanoes.
+volcano_list = ["Mayon", "Krakatoa", "Etna", "Cotopaxi", "Pelee", "Pinatubo", "Tambora"]
+
+#2. Print the entire list.
+print(volcano_list)
+
+#3. Access and print the 4th index.
+print(volcano_list[3])
+
+#4. Update the 3rd index to "Mount Vesuvius".
+volcano_list[2] = "Mount Vesuvius"
+print(volcano_list)
+
+#5. Delete the 5th index.
+del volcano_list[4]
+print(volcano_list)
+
+#6. Print the last index.
+print("The last index is: ", volcano_list[-1])
