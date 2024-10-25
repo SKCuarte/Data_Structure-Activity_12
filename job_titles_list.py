f54761a3530e771798103dd9df7686151d4acab4
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:56:57 +0800

    job_titles_list

---
+++ job_titles_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 job titles.
+job_list = ["Architect", "Pilot", "Judge", "Attorney", "Nurse", "Doctor", "Teacher", "Banker", "Barber", "Plumber"]
+
+#2. Print the entire list.
+print(job_list)
+
+#3. Access and print the 6th index.
+print(job_list[5])
+
+#4. Update the 5th index to "Software Engineer".
+job_list[4] = "Software Engineer"
+print(job_list)
+
+#5. Delete the 9th index.
+del job_list[8]
+print(job_list)
+
+#6. Print the last index.
+print("The last index is: ", job_list[-1])
