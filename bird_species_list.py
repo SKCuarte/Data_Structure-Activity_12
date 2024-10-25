a53951de970bb00374e721fd50a7108417ffabaa
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:58:00 +0800

    bird_species_list

---
+++ bird_species_list.py
@@ -0,0 +1,28 @@
+#1. Create a list of 12 bird species.
+bird_list = ["Duck", "Raven", "Sparrow", "Grosbeak", "Turkey", "Hoatzin", "Finch", "Mallard", "Kagu", "Tern", "Wigeon", "Cuckoo"]
+
+#2. Print the entire list.
+print(bird_list)
+
+#3. Access and print the 9th index.
+print(bird_list[8])
+
+#4. Update the 6th index to "Eagle".
+bird_list[5] = "Eagle"
+print(bird_list)
+
+#5. Delete the 8th index.
+del bird_list[7]
+print(bird_list)
+
+#6. Slice the list from index 3 to 7.
+print(bird_list[2:6])
+#OR
+print(bird_list[2:7])
+
+#7. Print the last index.
+print("The last index is: ", bird_list[-1])
+
+
+
+
