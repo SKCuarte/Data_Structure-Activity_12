c60696114d86a6e5e7ff95c8646ec75990308768
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:01:25 +0800

    dinosaur_list

---
+++ dinosaur_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 8 dinosaur names.
+dinosaur_list = ["Spinosaurus", "Velociraptor", "Brachiosaurus", "Diplodocus", "Dilophosaurus", "Giganotosaurus", "Sauroposeidon", "Titanosauria"]
+
+#2. Print the entire list.
+print(dinosaur_list)
+
+#3. Access and print the 4th index.
+print(dinosaur_list[3]) 
+
+#4. Update the 6th index to "Tyrannosaurus Rex".
+dinosaur_list[5] = "Tyrannosaurus Rex"
+print(dinosaur_list)
+
+#5. Delete the 7th index.
+del dinosaur_list[6]
+print(dinosaur_list)
+
+#6. Print the last index.
+print("The last index is: ", dinosaur_list[-1])
