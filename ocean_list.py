071868f60674812851592f69008802bad90927a8
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:57:24 +0800

    ocean_list

---
+++ ocean_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 5 oceans.
+ocean_list = ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Arctic Ocean"]
+
+#2. Print the entire list.
+print(ocean_list)
+
+#3. Access and print the 3rd index.
+print(ocean_list[2])
+
+#4. Update the 2nd index to "Pacific Ocean".
+ocean_list[1] = "Pacific Ocean"
+print(ocean_list)
+
+#5. Delete the 4th index.
+del ocean_list[3]
+print(ocean_list)
+
+#6. Print the last index.
+print("the last index is: ", ocean_list[-1])
