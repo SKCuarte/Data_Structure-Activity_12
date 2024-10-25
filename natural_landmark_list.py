29c13ff81f9a6a6d574668535e01a78df5f9d401
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:02:02 +0800

    natural_landmark_list

---
+++ natural_landmark_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 natural landmarks.
+landmark_list = ["Eiffel Tower", "Statue of Liberty", "Great Wall of China", "Tower of Pisa", "Colosseum", "Big Ben", "Great Sphinx of Giza", "Golden Gate Bridge", "Petra", "Times Square"] 
+
+#2. Print the entire list.
+print(landmark_list)
+
+#3. Access and print the 8th index.
+print(landmark_list[7])
+
+#4. Update the 5th index to "Grand Canyon".
+landmark_list[4] = "Grand Canyon"
+print(landmark_list)
+
+#5. Delete the 9th index.
+del landmark_list[8]
+print(landmark_list)
+
+#6. Print the last index.
+print("The last index is: ", landmark_list[-1])
