e28dc50f1f1591b307b17347d43420741f50ddad
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:00:22 +0800

    festival_celebration_list

---
+++ festival_celebration_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 festival celebrations.
+celebration_list = ["Carnival", "Dia de los Muertos", "MassKara Festival", "Panagbenga Festival", "Pahiyas Festival", "Venice Carniva", "Dinagyang Festival", "Kadayawa Festival", "Moriones Festival", "Film Festival"] 
+
+#2. Print the entire list.
+print(celebration_list)
+
+#3. Access and print the 5th index.
+print(celebration_list[4])
+
+#4. Update the 3rd index to "Thanksgiving".
+celebration_list[2] = "Thanksgiving"
+print(celebration_list)
+
+#5. Delete the 7th index.
+del celebration_list[6]
+print(celebration_list)
+
+#6. Print the last index.
+print("The last index is: ", celebration_list[-1])
