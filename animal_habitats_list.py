1bb7c81f238360b80a3abf35cfb9ea35d46b9856
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:55:54 +0800

    animal_habitats_list

---
+++ animal_habitats_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 8 animal habitats.
+habitat_list = ["Polar", "Desert", "Mountain", "Temperate Forest", "Freshwater", "Ocean", "Rainforest", "Grasslands"] 
+
+#2. Print the entire list.
+print(habitat_list)
+
+#3. Access and print the 5th index.
+print(habitat_list[4])
+
+#4. Update the 3rd index to "Savannah".
+habitat_list[2] = "Savannah"
+print(habitat_list)
+
+#5. Delete the 6th index.
+del habitat_list[5]
+print(habitat_list)
+
+#6. Print the last index.
+print("The last index is: ", habitat_list[-1])
