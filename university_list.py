2ab8c8570315ebe62ed03bee1708c45c4ac7f7be
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:59:52 +0800

    university_list

---
+++ university_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 universities.
+university_list = ["Stanford", "Yale", "CalTech", "John Hopkins", "Columbia", "Washington", "Duke", "Cornell", "Oxford", "New York"]
+
+#2. Print the entire list.
+print(university_list)
+
+#3. Access and print the 6th index.
+print(university_list[5])
+
+#4. Update the 4th index to "Harvard University".
+university_list[3] = "Harvard University"
+print(university_list)
+
+#5. Delete the 9th index.
+del university_list[8]
+print(university_list)
+
+#6. Print the last index.
+print("The last index is: ", university_list[-1])
