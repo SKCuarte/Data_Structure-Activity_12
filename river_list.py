2f825804812344eeb4985b24dce8fdcd1faadb4d
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:03:05 +0800

    river_list

---
+++ river_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 famous rivers.
+river_list = ["Yellow River", "Amazon River", "Yenisei River", "Yangtze River", "Mississippi River", "Ob River", "Congo River", "Irtysh River", "Missouri River", "Ghaghara River"] 
+
+#2. Print the entire list.
+print(river_list)
+
+#3. Access and print the 6th index.
+print(river_list[5])
+
+#4. Update the 8th index to "Nile River".
+river_list[7] = "Nile River"
+print(river_list)
+
+#5. Delete the 9th index.
+del river_list[8]
+print(river_list)
+
+#6. Print the last index.
+print("The last index is: ", river_list[-1])
