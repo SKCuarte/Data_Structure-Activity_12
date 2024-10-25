1d1fd2c0346a08e42ae836f3ad17d6aa031b90de
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:54:28 +0800

    technology_inventions_list

---
+++ technology_inventions_list.py
@@ -0,0 +1,24 @@
+#1. Create a list of 15 modern technology inventions.
+tech_list = ["Internet", "Electricity", "Car", "3D Printing", "Steam Engine", "Kevlar", "Wind Turbine", "Robotics", "Telephone", "Computer", "Laptop", "Film", "Camera", "Lens", "Augmented Reality"]
+
+#2. Print the entire list.
+print(tech_list)
+
+#3. Access and print the 11th index.
+print(tech_list[10])
+
+#4. Update the 8th index to "Smartphone".
+tech_list[7] = "Smartphone"
+print(tech_list)
+
+#5. Delete the 14th index.
+del tech_list[13]
+print(tech_list)
+
+#6. Slice the list from index 5 to 10.
+print(tech_list[4:9])
+#OR
+print(tech_list[4:10])
+
+#7. Print the last index.
+print("The last index is: ", tech_list[-1])
