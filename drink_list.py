f849df567c3747403f41aa44a90c6a7ef01607f1
Kyan <kyancuarte22@yahoo.com>
2024-10-25 23:59:24 +0800

    drink_list

---
+++ drink_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 8 popular drinks.
+drink_list = ["Coke", "Royal", "Sprite", "Rootbeer", "Wine", "Gin", "Mountain Dew", "Water"]
+
+#2. Print the entire list.
+print(drink_list)
+
+#3. Access and print the 4th index.
+print(drink_list[3])
+
+#4. Update the 3rd index to "Coffee".
+drink_list[2] = "Coffee"
+print(drink_list)
+
+#5. Delete the 7th index.
+del drink_list[6]
+print(drink_list)
+
+#6. Print the last index.
+print("The last index is: ", drink_list[-1])
