0b35ac3df093f224d03d3f84be7fc15f00b72d4a
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:03:45 +0800

    waterfall_list

---
+++ waterfall_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 10 famous waterfalls.
+waterfall_list = ["Victoria Falls", "Gullfoss Falls", "Iguaza Falls", "Yosemite Falls", "Ban Gioc Waterfalls", "Tugela Waterfalls", "NoKaLikai Falls", "Jog Falls", "Hannoki Falls", "Tinago Falls"]
+
+#2. Print the entire list.
+print(waterfall_list)
+
+#3. Access and print the 8th index.
+print(waterfall_list[7])
+
+#4. Update the 5th index to "Niagara Falls".
+waterfall_list[4] = "Niagra Falls"
+print(waterfall_list)
+
+#5. Delete the 9th index.
+del waterfall_list[8]
+print(waterfall_list)
+
+#6. Print the last index.
+print("The last index is: ", waterfall_list[-1])
