a80f7cf52c0c0a845953abf44526b676ae46f175
Kyan <kyancuarte22@yahoo.com>
2024-10-26 00:04:37 +0800

    world_wonders_list

---
+++ world_wonders_list.py
@@ -0,0 +1,19 @@
+#1. Create a list of 7 wonders of the world.
+wonder_list = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Temple of Artemis", "Mausoleum at Halicarnassus", "Stonehenge", "Acropolis of Athens", "Hagia Sophia"] 
+
+#2. Print the entire list.
+print(wonder_list)
+
+#3. Access and print the 4th index.
+print(wonder_list[3])
+
+#4. Update the 2nd index to "Great Wall of China".
+wonder_list[1] = "Great Wall of China"
+print(wonder_list)
+
+#5. Delete the 5th index.
+del wonder_list[4]
+print(wonder_list)
+
+#6. Print the last index.
+print("The last index is: ", wonder_list[-1])
